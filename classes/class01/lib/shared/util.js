4e7a1ac9c3efe136ee7a0d7d5a17aaeba1734fda
Clovis Carmezini <cloviscarmezini14@gmail.com>
2023-03-15 22:38:04 -0300

    first commit

---
+++ classes/class01/lib/shared/util.js
@@ -0,0 +1,35 @@
+function supportsWorkerType() {
+    let supports = false
+    const tester = {
+        get type() {
+            supports = true
+        }
+    }
+
+    try {
+        new Worker('blob://', tester).terminate()
+    } finally {
+        return supports
+    }
+}
+
+function prepareRunChecker({ timerDelay }) {
+    let lasEvent = Date.now()
+
+    return {
+        shouldRun() {
+            const result = (Date.now() - lasEvent) > timerDelay;
+
+            if(result) {
+                lasEvent = Date.now();
+            }
+
+            return result;
+        }
+    }
+}
+
+export {
+    supportsWorkerType,
+    prepareRunChecker
+}
